590fe5a207ad1f7e93eea9cda6ef4a33a7d9162e
GiulianoMaPe <giulianomape.1806.14@gmail.com>
2024-03-31 16:38:06 -0500

    Ají Amarillo - ARREGLOS2

--- src/main/java/net/giuliano/peruviansdelight/block/custom/AjiAmarilloCropBlock.java
+++ src/main/java/net/giuliano/peruviansdelight/block/custom/AjiAmarilloCropBlock.java
@@ -1,20 +1,63 @@
 package net.giuliano.peruviansdelight.block.custom;
 
 import net.giuliano.peruviansdelight.item.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class AjiAmarilloCropBlock extends CropBlock {
     public static final int MAX_AGE = 7;
     public static final IntegerProperty AGE = BlockStateProperties.AGE_7;
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{
+            Block.box(4.0D, 0.0D, 4.0D, 12.0D, 7.0D, 12.0D), Block.box(3.0D, 0.0D, 3.0D, 13.0D, 10.0D, 13.0D),
+            Block.box(3.0D, 0.0D, 3.0D, 13.0D, 13.0D, 13.0D), Block.box(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D),
+            Block.box(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D), Block.box(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D),
+            Block.box(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D), Block.box(2.0D, 0.0D, 2.0D, 14.0D, 15.0D, 14.0D)};
 
     public AjiAmarilloCropBlock(Properties pProperties) {
         super(pProperties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return SHAPE_BY_AGE[this.getAge(pState)];
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        int i = pState.getValue(AGE);
+        boolean flag = i == 7;
+        if (!flag && pPlayer.getItemInHand(pHand).is(Items.BONE_MEAL)) {
+            return InteractionResult.PASS;
+        } else if (i > 6) {
+            int j = 2 + pLevel.random.nextInt(2);
+            popResource(pLevel, pPos, new ItemStack(ModItems.AJI_AMARILLO.get(), j + (flag ? 1 : 0)));
+            pLevel.playSound((Player)null, pPos, SoundEvents.SWEET_BERRY_BUSH_PICK_BERRIES, SoundSource.BLOCKS, 1.0F, 0.8F + pLevel.random.nextFloat() * 0.4F);
+            BlockState blockstate = pState.setValue(AGE, Integer.valueOf(3));
+            pLevel.setBlock(pPos, blockstate, 2);
+            pLevel.gameEvent(GameEvent.BLOCK_CHANGE, pPos, GameEvent.Context.of(pPlayer, blockstate));
+            return InteractionResult.sidedSuccess(pLevel.isClientSide);
+        } else {
+            return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
+        }
     }
 
     @Override
