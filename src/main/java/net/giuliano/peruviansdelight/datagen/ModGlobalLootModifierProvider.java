53215cb6d431ff73f2af6d3fc8045afd3c1537ef
GiulianoMaPe <giulianomape.1806.14@gmail.com>
2025-08-01 16:38:22 -0500

    Modificación de loot Correcto

--- src/main/java/net/giuliano/peruviansdelight/datagen/ModGlobalLootModifierProvider.java
+++ src/main/java/net/giuliano/peruviansdelight/datagen/ModGlobalLootModifierProvider.java
@@ -27,12 +27,103 @@ public class ModGlobalLootModifierProvider  extends GlobalLootModifierProvider {
         }, ModItems.INCA_KOLA.get()));
         */
 
-        add("lana_for_llama", new AddItemModifier(new LootItemCondition[] {
-                new LootTableIdCondition.Builder(new ResourceLocation("entities/llama")).build(),
-        }, ModItems.INCA_KOLA.get()));
+        add("lana_from_llama_x_color", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("entities/llama")).build()
+        }));
 
         add("inka_for_chest1", new AddItemModifier(new LootItemCondition[] {
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/pillager_outpost")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest2", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/desert_pyramid")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest3", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/abandoned_mineshaft")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest4", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/stronghold_library")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest5", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/shipwreck_treasure")).build(),
+                LootItemRandomChanceCondition.randomChance(0.10f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest6", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/ruined_portal")).build(),
+                LootItemRandomChanceCondition.randomChance(0.10f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest7", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/igloo_chest")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest8", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/buried_treasure")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest9", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/nether_bridge")).build(),
+                LootItemRandomChanceCondition.randomChance(0.10f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest10", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/end_city_treasure")).build(),
+                LootItemRandomChanceCondition.randomChance(0.30f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest11", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/ancient_city")).build(),
+                LootItemRandomChanceCondition.randomChance(0.30f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest12", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest13", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/village/village_armorer")).build(),
+                LootItemRandomChanceCondition.randomChance(0.05f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest14", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/village/village_butcher")).build(),
+                LootItemRandomChanceCondition.randomChance(0.30f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest15", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_bridge")).build(),
+                LootItemRandomChanceCondition.randomChance(0.05f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest16", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/bastion_treasure")).build(),
+                LootItemRandomChanceCondition.randomChance(0.10f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest17", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/woodland_mansion")).build(),
+                LootItemRandomChanceCondition.randomChance(0.50f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest18", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/underwater_ruin_big")).build(),
+                LootItemRandomChanceCondition.randomChance(0.25f).build(),
+        }, ModItems.INCA_KOLA.get()));
+
+        add("inka_for_chest19", new AddItemModifier(new LootItemCondition[] {
+                new LootTableIdCondition.Builder(new ResourceLocation("chests/underwater_ruin_small")).build(),
+                LootItemRandomChanceCondition.randomChance(0.10f).build(),
         }, ModItems.INCA_KOLA.get()));
     }
 }
--- src/main/java/net/giuliano/peruviansdelight/loot/AddItemModifier.java
+++ src/main/java/net/giuliano/peruviansdelight/loot/AddItemModifier.java
@@ -4,39 +4,65 @@ import com.google.common.base.Suppliers;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.world.entity.animal.horse.Llama;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
-public class AddItemModifier  extends LootModifier {
+public class AddItemModifier extends LootModifier {
     public static final Supplier<Codec<AddItemModifier>> CODEC = Suppliers.memoize(()
-        -> RecordCodecBuilder.create(inst -> codecStart(inst).and(ForgeRegistries.ITEMS.getCodec()
-            .fieldOf("item").forGetter(m -> m.item)).apply(inst, AddItemModifier::new)));
+            -> RecordCodecBuilder.create(inst -> codecStart(inst).and(
+            ForgeRegistries.ITEMS.getCodec().optionalFieldOf("item").forGetter(m -> Optional.ofNullable(m.item))
+    ).apply(inst, AddItemModifier::new)));
+
+    @Nullable
     private final Item item;
 
-    public AddItemModifier(LootItemCondition[] conditionsIn, Item item) {
+    public AddItemModifier(LootItemCondition[] conditionsIn, Optional<Item> item) {
         super(conditionsIn);
-        this.item = item;
+        this.item = item.orElse(null);
+    }
+    public AddItemModifier(LootItemCondition[] conditionsIn, Item item) {
+        this(conditionsIn, Optional.of(item));
+    }
+    public AddItemModifier(LootItemCondition[] conditionsIn) {
+        this(conditionsIn, Optional.empty());
     }
 
     @Override
     protected @NotNull ObjectArrayList<ItemStack> doApply(ObjectArrayList<ItemStack> generatedLoot, LootContext context) {
-        for(LootItemCondition condition : this.conditions) {
-            if(!condition.test(context)) {
-                return generatedLoot;
+        if (this.item != null) {
+            generatedLoot.add(new ItemStack(this.item));
+        } else {
+            if (context.hasParam(LootContextParams.THIS_ENTITY) && context.getParam(LootContextParams.THIS_ENTITY) instanceof Llama llama) {
+                Item woolItem = getWoolForVariant(llama.getVariant());
+                if (woolItem != null) {
+                    generatedLoot.add(new ItemStack(woolItem, 1));
+                }
             }
         }
 
-        generatedLoot.add(new ItemStack(this.item));
-
         return generatedLoot;
+    }
+
+    private static Item getWoolForVariant(Llama.Variant variant) {
+        return switch (variant) {
+            case CREAMY, WHITE -> Blocks.WHITE_WOOL.asItem();
+            case BROWN -> Blocks.BROWN_WOOL.asItem();
+            case GRAY -> Blocks.GRAY_WOOL.asItem();
+            default -> null;
+        };
     }
 
     @Override
