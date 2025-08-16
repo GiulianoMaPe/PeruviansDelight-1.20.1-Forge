67445efb3fe248e158544b100cd1ac7f4ec0b94a
GiulianoMaPe <giulianomape.1806.14@gmail.com>
2025-08-16 18:56:35 -0500

    Las vacas sueltan su corazón

--- src/main/java/net/giuliano/peruviansdelight/event/ModEvents.java
+++ src/main/java/net/giuliano/peruviansdelight/event/ModEvents.java
@@ -1,9 +1,35 @@
 package net.giuliano.peruviansdelight.event;
 
 import net.giuliano.peruviansdelight.PeruviansDelight;
+import net.giuliano.peruviansdelight.item.ModItems;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = PeruviansDelight.MOD_ID)
 public class ModEvents {
 
+    private static final TagKey<Item> KNIVES_TAG = ItemTags.create(new ResourceLocation("forge", "tools/knives"));
+
+    @SubscribeEvent
+    public static void onLivingDrops(LivingDropsEvent event) {
+        if (event.getEntity() instanceof Cow cow) {
+            if (event.getSource().getEntity() instanceof Player player) {
+                ItemStack weapon = player.getMainHandItem();
+
+                if (weapon.is(KNIVES_TAG)) {
+                    event.getDrops().add(cow.spawnAtLocation(new ItemStack(ModItems.BEEF_HEART.get())));
+                }
+            }
+        }
+    }
 }
